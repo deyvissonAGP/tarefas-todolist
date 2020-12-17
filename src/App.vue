aa9ef4cd91172869dc3366c3a51fa2ce466fa2b5
Deyvisson Augusto <deyvisson@stc.ma.gov.br>
2020-12-17 15:40:22 -0300

    Componente Tarefa 03

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 	<div id="app">
 		<h1>Tarefas</h1>
 		<NewTask @taskAdded="addTask" />
-    	<TaskGrid @taskDeleted="deleteTask" :tasks="tasks" />
+    	<TaskGrid :tasks="tasks" 
+			@taskDeleted="deleteTask" 
+			@taskStateChanged="toggleTaskState" />
 	</div>
 </template>
 
@@ -30,6 +32,9 @@ export default {
 		},
 		deleteTask(i) {
 			this.tasks.splice(i, 1)
+		},
+		toggleTaskState(i) {
+			this.tasks[i].pending = !this.tasks[i].pending
 		}
 	}
 }
